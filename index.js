b8c1cf89fc69476b2face7a6dfcc3b66983b6adf
Jherico Solier <74070374+JhericoS@users.noreply.github.com>
2023-02-17 09:04:35 -0500

    version 1.0

--- index.js
+++ index.js
@@ -2,15 +2,21 @@
 
 const buttonAdd = document.getElementsByName('data-add')[0];
 const buttonSub = document.getElementsByName('data-sub')[0];
+const buttonAddY = document.getElementsByName('data-add-y')[0];
+const buttonSubY = document.getElementsByName('data-sub-y')[0];
 
 // Initial variables
 
 var count = document.getElementById('count');
+var years = document.getElementById('years');
 var result = document.getElementById('result');
-count.value = 1;
-result.value = 230*count.value;
+count.value = 5;
+years.value = 1;
+result.value = 18.5*count.value*years.value;
 
 // Buttons functions
+
+// Add pc's
 
 buttonAdd.addEventListener('click', function(){
     calculateAdd();
@@ -18,11 +24,13 @@ buttonAdd.addEventListener('click', function(){
 });
 
 function calculateAdd(){
-    if (count.value < 10) {
+    if (count.value < 100) {
         const previous = parseFloat(count.value);
         count.value = previous + 1;
     }
 }
+
+// Sub pc's
 
 buttonSub.addEventListener('click', function(){
     calculateSub();
@@ -30,13 +38,50 @@ buttonSub.addEventListener('click', function(){
 });
 
 function calculateSub(){
-    if (count.value > 1) {
+    if (count.value > 5) {
         const previous = parseFloat(count.value);
         count.value = previous - 1;
     }
 }
 
+// Add Years
+
+buttonAddY.addEventListener('click', function(){
+    calculateAddY();
+    refreshPrice();
+});
+
+function calculateAddY(){
+    if (years.value < 3) {
+        const previous = parseFloat(years.value);
+        years.value = previous + 1;
+    }
+}
+
+// Sub Years
+
+buttonSubY.addEventListener('click', function(){
+    calculateSubY();
+    refreshPrice();
+});
+
+function calculateSubY(){
+    if (years.value > 1) {
+        const previous = parseFloat(years.value);
+        years.value = previous - 1;
+    }
+}
+
 function refreshPrice(){
     const previous = parseFloat(count.value);
-    result.value = previous * 230;
+    const timey = parseFloat(years.value);
+    if (previous <= 10){
+    result.value = previous * 18.5 * timey;
+    }
+    else if (previous >= 11 && previous<=25){
+        result.value = previous * 15.5 * timey;
+    }
+    else if (previous >= 26 && previous<=49){
+        result.value = previous * 13.5 * timey;
+    }
 }
